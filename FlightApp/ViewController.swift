ae9c0bd7538dbd450f0211e841eff65d2a59245f
Atinderpal Singh <atinder007.singh@gmail.com>
2019-06-29 11:04:00 +0530

    First Commit

---
+++ FlightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlightApp
+//
+//  Created by Atinderpal Singh on 6/29/19.
+//  Copyright © 2019 Reliance Jio Infocomm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
